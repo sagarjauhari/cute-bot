3e64baee1cb821e347390f8077d7eebf05c27c4a
Stephen Eckenrode <seckenrode@panoramaed.com>
2015-08-01 09:23:30 -0400

    First Version

--- web.js
+++ web.js
@@ -1,36 +1,62 @@
-// Require the 'ac-koa' module, and then tell it to load the 'hipchat' adapter
-// from 'ac-koa-hipchat'
+Error.stackTraceLimit = Infinity;
+
 var ack = require('ac-koa').require('hipchat');
-// Require our package.json file, which doubles as the configuration from which
-// we'll generate the add-on descriptor and server's runtime parameters
+var request = require('koa-request');
+var crypto = require('crypto');
 var pkg = require('./package.json');
-// Create the base Koa app, via an 'ac-koa' factory method that helps preconfigure
-// and decorate the app object
+
 var app = ack(pkg);
 
-// Now build and mount an AC add-on on the Koa app; we can either pass a full or
-// partial descriptor object to the 'addon()' method, or when we provide none, as
-// in this example, we can instead create the descriptor using a product-specific
-// builder API
-var addon = app.addon()
-  // Use the hipchat descriptor builder
-  .hipchat()
-  // Indicate that the descriptor should mark this as installable in rooms
-  .allowRoom(true)
-  // Provide the list of permissions scopes the add-on requires
-  .scopes('send_notification');
+var addon = app.addon().
+            hipchat().
+            allowRoom(true).
+            scopes('send_notification');
 
-// Subscribe to the 'room_enter' webhook, and provide an event listener.  Under
-// the covers, this adds a webhook entry to the add-on descriptor, mounts a common
-// webhook endpoint on the Koa app, and brokers webhook POST requests to the event
-// listener as appropriate
-addon.webhook('room_enter', function *() {
-  // 'this' is a Koa context object, containing standard Koa request and response
-  // contextual information as well as hipchat-specific models and services that
-  // make handling the webhook as simple as possible
+addon.webhook('room_message', /^\/hello$/, function *() {
   yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!');
 });
 
-// Now that the descriptor has been defined along with a useful webhook handler,
-// start the server 
+function randomValueHex (len) {
+    return crypto.randomBytes(Math.ceil(len/2)).
+           toString('hex').
+           slice(0, len);
+}
+
+var cuteMe = function *() {
+  var cacheBust = "_cache=" + randomValueHex(8);
+
+  var subreddits = [
+    "aww",
+    "awwgifs",
+    "babybeastgifs",
+    "corgis",
+    "goldenretrievers",
+    "puppies",
+    "Rabbits"
+  ];
+
+  var url = "http://reddit.com/r/" +  
+            subreddits.join("+") +
+            "/random.json?" +
+            cacheBust
+
+  var requestOptions = {
+    url: url,
+    headers: {
+      "User-Agent": "cute-bot/v0.1.0 (by sre9981)",
+      "Cache-Control": "no-cache"
+    }
+  };
+
+  var response = yield request(requestOptions);
+  var post = JSON.parse(response.body)[0].data.children[0];
+  var message = "<a href='" + post.data.url + "'>" +
+                "<img src='" + post.data.thumbnail + "'/>" + 
+                "<br />" + post.data.url + "</a>"
+
+  yield this.roomClient.sendNotification(message);
+};
+
+addon.webhook('room_message', /^\/cute$/, cuteMe);
+
 app.listen();
